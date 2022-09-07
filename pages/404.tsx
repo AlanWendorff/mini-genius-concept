267d3c23833700a64c3254b94c50ebe6315facd3
unknown <alanwff4@gmail.com>
2022-09-07 17:11:48 -0300

    added custom 404 screen

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const NotFoundPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Custom 404 page not found</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
