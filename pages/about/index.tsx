c6582bc7a117e527aa577502a4bb48e1eadce12c
Alan Wendorff <62715512+Kremowy@users.noreply.github.com>
2022-09-07 15:25:07 -0300

    Add files via upload

---
+++ pages/about/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const index: NextPage = () => {
+  return (
+    <div>
+      <h1>This is an about screen. Here i want to explain the technologies.</h1>
+    </div>
+  );
+};
+
+export default index;
---
+++ pages/index/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const index: NextPage = () => {
+  return (
+    <div>
+      <h1>This is 9z match.ez</h1>
+    </div>
+  );
+};
+
+export default index;
