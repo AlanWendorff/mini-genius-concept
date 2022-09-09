e06ed0be16882f90e900d16c02b41eed30075c73
unknown <alanwff4@gmail.com>
2022-09-09 13:25:07 -0300

    added web information component

---
+++ components/web/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const index: NextPage = () => {
+  return (
+    <Head>
+      <title>Next Match.ez</title>
+    </Head>
+  );
+};
+
+export default index;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 import type { AppProps } from "next/app";
 import NavBar from "@components/ui/nav-bar";
+import Web from "@components/web";
 import "@styles/globals.scss";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
+    <Web />
     <Component {...pageProps} />
     <NavBar />
   </>
