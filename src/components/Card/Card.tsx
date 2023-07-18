1db699d607dd58b93f148b5a1928f11ae91fd9fa
Alan Wendorff <alanwff4@gmail.com>
2023-07-18 13:16:04 -0300

    finished card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,6 @@
 import IMAGE from "../../assets/images/dvsr-logo.jpg";
 import styles from "./Card.module.scss";
+import ALBUMS from "./data";
 
 const Card = () => (
   <div className={styles.container}>
@@ -9,18 +10,33 @@ const Card = () => (
       <p className={styles.subtitle}>
         AKA: Designed via Strength & Respect, Devastator
       </p>
+    </div>
 
-      {/* <div className={styles.about}>
-        <h3>About "DVSR"</h3>
+    <div className={styles.bandInfo}>
+      <div className={styles.about}>
+        <h3>About “DVSR“</h3>
 
         <p>
           <span>DVSR</span> are a fusion of Rap/Hip Hop and Metal. Combing these
           two styles to create one of its own.
         </p>
-      </div> */}
-    </div>
+      </div>
 
-    <div className={styles.albums}>albums</div>
+      <div className={styles.albums}>
+        <h3>POPULAR DVSR ALBUMS</h3>
+        <div className={styles.grid}>
+          {ALBUMS.map(({ id, image, name, release_year }) => (
+            <div key={id} className={styles.album}>
+              <img src={image} alt={name} />
+              <div>
+                <p>{name}</p>
+                <p className={styles.year}>{release_year}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   </div>
 );
 
