0bf3efe5759219d2d408925a07ac5bbed7641f1b
Alan Wendorff <alanwff4@gmail.com>
2023-07-21 01:20:02 -0300

    fixed data

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from "next";
 
 const NotFoundPage: NextPage = () => (
   <div className="notFound">
-    <h1>I'm not a Genius but</h1>
-    <h1>I think that something isn't workin'</h1>
+    <h1>I&lsquo;m not a Genius but</h1>
+    <h1>I think that something isn&lsquo;t workin&lsquo;</h1>
     <h1>404</h1>
   </div>
 );
