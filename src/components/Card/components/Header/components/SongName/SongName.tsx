c3af39a6f638263ac784c494f98d90f671ea09b7
Alan Wendorff <alanwff4@gmail.com>
2023-07-23 18:42:50 -0300

    style fixes

--- src/components/Card/components/Header/components/SongName/SongName.tsx
+++ src/components/Card/components/Header/components/SongName/SongName.tsx
@@ -15,45 +15,39 @@ const SongName = ({
   handleReturnAlbum,
   handleReturnMenu,
 }: props) => (
-  <div className={styles.container}>
-    <AnimatePresence>
-      {show && (
-        <motion.div
-          key="song-name"
-          className={styles.animatedContainer}
-          initial={{ x: -300 }}
-          animate={{ x: 0 }}
-          exit={{ x: -300 }}
-          transition={{ type: "tween" }}
-        >
-          <h1>{selectedTrack?.name}</h1>
+  <AnimatePresence>
+    {show && (
+      <motion.div
+        key="song-name"
+        className={styles.animatedContainer}
+        initial={{ x: -300 }}
+        animate={{ x: 0 }}
+        exit={{ x: -300 }}
+        transition={{ type: "tween" }}
+      >
+        <h1>{selectedTrack?.name}</h1>
 
-          <div className={styles.songInfo}>
-            <a
-              onClick={handleReturnMenu}
-              role="button"
-              aria-label="back to band"
-            >
-              {selectedTrack?.band}
-            </a>
-            &nbsp;
-            <p>• Track {selectedTrack?.id} on</p>
-            &nbsp;
-            <a
-              onClick={handleReturnAlbum}
-              role="button"
-              aria-label="back to album"
-            >
-              <p>{selectedTrack?.album}&nbsp;&nbsp;&nbsp;&nbsp;</p>
-              <svg viewBox="0 0 6.6 16">
-                <path d="M1.6 8.8l.6-.6 1 1 .5.7V6H0v-.8h4.5v4.6l.5-.6 1-1 .6.5L4 11.3 1.6 8.8z"></path>
-              </svg>
-            </a>
-          </div>
-        </motion.div>
-      )}
-    </AnimatePresence>
-  </div>
+        <div className={styles.songInfo}>
+          <a onClick={handleReturnMenu} role="button" aria-label="back to band">
+            {selectedTrack?.band}
+          </a>
+          &nbsp;
+          <p>• Track {selectedTrack?.id} on</p>
+          &nbsp;
+          <a
+            onClick={handleReturnAlbum}
+            role="button"
+            aria-label="back to album"
+          >
+            <p>{selectedTrack?.album}&nbsp;&nbsp;&nbsp;&nbsp;</p>
+            <svg viewBox="0 0 6.6 16">
+              <path d="M1.6 8.8l.6-.6 1 1 .5.7V6H0v-.8h4.5v4.6l.5-.6 1-1 .6.5L4 11.3 1.6 8.8z"></path>
+            </svg>
+          </a>
+        </div>
+      </motion.div>
+    )}
+  </AnimatePresence>
 );
 
 export default SongName;
